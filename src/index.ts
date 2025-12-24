d7c0091f65486188518be6971f960f5840da361e
uvenkatateja <uvteja1111@gmail.com>
2025-12-25 01:14:57 +0530

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { configCmd } from './core/config-cmd.js';
 cli(
   {
     name: 'rewordcommit',
-    version: '1.0.2',
+    version: '1.0.1',
     flags: {
       quick: {
         type: Boolean,
